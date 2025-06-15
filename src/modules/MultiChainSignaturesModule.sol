5e1334c84c783ca237bc467be0fc9b2ce0dca3b5
jordan <41rjordan@proton.me>
2025-06-15 14:40:09 +0500

    init multichain signatures module

---
+++ src/modules/MultiChainSignaturesModule.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity >=0.7.0 <0.9.0;
+
+import "lib/safe-smart-account/contracts/interfaces/ISignatureValidator.sol";
+
+contract MultiChainSignaturesModule {
+
+}
